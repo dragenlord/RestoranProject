7d042a73954eec98ca7bbfc567458aedd1d2ffe4
Dragenlord <K.Malkin2009@yandex.ru>
2025-05-28 09:41:57 +0300

    добавил напитки, корзину, профиль, снеки, пиццу.

--- src/main/java/com/example/Restoran/controller/MyPageController.java
+++ src/main/java/com/example/Restoran/controller/MyPageController.java
@@ -6,18 +6,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/page")
+@RequestMapping("/api")
 public class MyPageController {
 
-    @GetMapping("/menu")
+    @GetMapping("/page")
     public String paintMyPage(){
-
         return "MyPage";
+    }
+
+    @GetMapping("/cart")
+    public String paintcart(){
+        return "cart";
+    }
+
+    @GetMapping("/drinks")
+    public String paintdrinks(){
+        return "drinks";
+
+    }
+    @GetMapping("/pizza")
+    public String paintpizza(){
+        return "pizza";
+    }
+
+    @GetMapping("/profile")
+    public String paintprofile(){
+        return "profile";
 
     }
 
+    @GetMapping("/snacks")
+    public String paintsneki(){
+        return "sneki";
 
+    }
 
+    @GetMapping("/sushi")
+    public String paintsushi(){
+        return "sushi";
+
+    }
 
 
 }
