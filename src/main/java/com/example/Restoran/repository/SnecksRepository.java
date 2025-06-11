65df3642df11ffd56fec3e86f2130bb486b636b5
Dragenlord <K.Malkin2009@yandex.ru>
2025-06-11 15:26:51 +0300

    добавил модули для работы с доставкой и уведомлениями

--- src/main/java/com/example/Restoran/repository/SnecksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Restoran.repository;
-
-import com.example.Restoran.entity.snecks;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SnecksRepository extends JpaRepository<snecks, Integer> {
-}
